41c00ee6310135437bcda3f527e7cbe21826b815
Michael George <michael.g.george@gmail.com>
2022-08-03 02:27:17 +0530

    Add todo class and constructor

---
+++ src/todo.js
@@ -0,0 +1,16 @@
+export default class ToDo{
+    #title;
+    #description;
+    #dueDate;
+    #priority;
+    #notes;
+    #checklist;
+    constructor(title, description, dueDate, priority, notes, checklist){
+        this.#title = title;
+        this.#description = description;
+        this.#dueDate = dueDate;
+        this.#priority = priority;
+        this.#notes = notes;
+        this.#checklist = checklist;
+    }
+};
